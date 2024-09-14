798a2e4ea3c9d4d3a5ec6cb3eba4ab884fe0eda3
gbabyblue <gabrielyudken@gmail.com>
2024-09-14 12:52:25 -0400

    fix gain

--- index.js
+++ index.js
@@ -620,7 +620,7 @@ function chooseRandomFrequency(min, size, callback) {
 function playOsc(osc, type, gain, sustain, freq) {
   let stopTime = Number(.4)
   let oscGain = audioCtx.createGain();
-  oscGain.gain.value = .3;
+  oscGain.gain.value = .2;
   osc = audioCtx.createOscillator();
   osc.type =  "sine"// type//"sawtooth" //"square";
   osc.frequency.setValueAtTime(freq[0], audioCtx.currentTime);
